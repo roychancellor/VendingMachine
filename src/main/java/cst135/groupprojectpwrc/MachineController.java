9d99405ae8a85cb757cc854e81913ac19beaf645
Roy Chancellor <roychance600@gmail.com>
2019-07-19 13:48:33 -0700

    Removed Paul's phone number
    * Preparing to hand off to new development team

--- src/main/java/cst135/groupprojectpwrc/MachineController.java
+++ src/main/java/cst135/groupprojectpwrc/MachineController.java
@@ -8,7 +8,7 @@ public class MachineController {
 	 */
 	public static void main(String[] args) {
 		//Make the two required objects to run the machine
-		Administrator root = new Administrator("<<rootkey>>", "6026894457");
+		Administrator root = new Administrator("<<rootkey>>", "1234567890");
 		VendingMachine vm = new VendingMachine(3, 3, root);
 		
 		//Initialize the machine (loads it with tasty items and sets to initialized)
